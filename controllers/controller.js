9e4135afc68141b7f1f8429ee6a3ebb57e248f0e
Rodrigo <roi31@hotmail.com>
2020-03-02 13:55:25 +0100

    Add files via upload

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,17 +1,28 @@
 "use strict";
 function createCar(plate, brand, color) {
-    var carInfo = document.querySelector("#carInfo");
-    var wheelsInfo = document.querySelector("#wheelsInfo");
-    var inputPlate = document.querySelector("#inputPlate");
-    var inputBrand = document.querySelector("#inputBrand");
-    var inputColor = document.querySelector("#inputColor");
-    plate = inputPlate.value;
-    var car = new Car(plate, inputBrand.value, inputColor.value);
-    //    let plateValue =  inputPlate.value;
-    console.log(plate);
-    car.addWheel(new Wheel(4, "SEAT"));
-    carInfo.classList.add("alert", "alert-info");
-    carInfo.innerHTML = "CAR:</br> PLATE:" + car.plate
+    // VARIABLES ---------------------------------------------------------------------------------
+    var carOutput = document.querySelector("#carInfo");
+    var wheelsOutput = document.querySelector("#wheelsInfo");
+    var wheelsInput = document.querySelector("#wheelsInput");
+    var inputPlate = document.querySelector("#inputPlate"), inputBrand = document.querySelector("#inputBrand"), inputColor = document.querySelector("#inputColor");
+    var brandWheel1 = document.querySelector("#brandWheel1"), brandWheel2 = document.querySelector("#brandWheel2"), brandWheel3 = document.querySelector("#brandWheel3"), brandWheel4 = document.querySelector("#brandWheel4"), diameterWheel1 = document.querySelector("#diameterWheel1"), diameterWheel2 = document.querySelector("#diameterWheel2"), diameterWheel3 = document.querySelector("#diameterWheel3"), diameterWheel4 = document.querySelector("#diameterWheel4");
+    // END VARIABLES -------------------------------------------------------------------------------
+    showInputWheels();
+    checkWheels();
+    var car = new Car(inputPlate.value, inputBrand.value, inputColor.value);
+    carOutput.classList.add("alert", "alert-info");
+    carOutput.innerHTML = "CAR:</br> PLATE:" + car.plate
         + "</br> COLOR: " + car.color + "</br> BRAND: " + car.brand;
-    wheelsInfo.innerHTML = "WHEELS: " + JSON.stringify(car.wheels);
+    // FUNCTIONS -----------------------------------------------------------------------------------
+    function showInputWheels() {
+        wheelsInput.classList.remove("invisible");
+        wheelsInput.classList.add("visible");
+    }
+    ;
+    function checkWheels() {
+        car.addWheel(new Wheel(diameterWheel1, brandWheel1.value));
+        wheelsOutput.innerHTML = "WHEELS: " + JSON.stringify(car.wheels);
+    }
+    ;
+    // END FUNCTIONS ---------------------------------------------------------------------------------
 }
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,27 +1,52 @@
 
+ 
 function createCar(plate:string,brand:string,color:string){
 
-    let carInfo: HTMLHeadingElement = document.querySelector("#carInfo") as HTMLHeadingElement;
-    let wheelsInfo: HTMLHeadingElement = document.querySelector("#wheelsInfo") as HTMLHeadingElement;
-    let inputPlate: any = document.querySelector("#inputPlate");
-    let inputBrand: any = document.querySelector("#inputBrand");
-    let inputColor: any = document.querySelector("#inputColor");
-    
-    plate = inputPlate.value;
+// VARIABLES ---------------------------------------------------------------------------------
+    let carOutput: HTMLHeadingElement = document.querySelector("#carInfo") as HTMLHeadingElement;
+    let wheelsOutput: HTMLHeadingElement = document.querySelector("#wheelsInfo") as HTMLHeadingElement;
+    let wheelsInput: HTMLHtmlElement = document.querySelector("#wheelsInput") as HTMLHtmlElement;
 
-    let car = new Car(plate,inputBrand.value,inputColor.value);
-    //    let plateValue =  inputPlate.value;
-    console.log(plate);
-    
-    car.addWheel(new Wheel(4,"SEAT"));
+    let inputPlate: any = document.querySelector("#inputPlate"),
+        inputBrand: any = document.querySelector("#inputBrand"),
+        inputColor: any = document.querySelector("#inputColor");
 
-    carInfo.classList.add("alert","alert-info");
-    
-    carInfo.innerHTML = "CAR:</br> PLATE:"  + car.plate 
+    let brandWheel1: string = document.querySelector("#brandWheel1"),
+        brandWheel2: string = document.querySelector("#brandWheel2"),
+        brandWheel3: any = document.querySelector("#brandWheel3"),
+        brandWheel4: any = document.querySelector("#brandWheel4"),
+        diameterWheel1: number = document.querySelector("#diameterWheel1"),
+        diameterWheel2: any = document.querySelector("#diameterWheel2"),
+        diameterWheel3: any = document.querySelector("#diameterWheel3"),
+        diameterWheel4: any = document.querySelector("#diameterWheel4");
+
+// END VARIABLES -------------------------------------------------------------------------------
+
+
+    showInputWheels();
+    checkWheels();
+
+    let car = new Car(inputPlate.value,inputBrand.value,inputColor.value);
+    carOutput.classList.add("alert","alert-info");
+    carOutput.innerHTML = "CAR:</br> PLATE:"  + car.plate 
     + "</br> COLOR: " + car.color + "</br> BRAND: " + car.brand;
+    
+    // FUNCTIONS -----------------------------------------------------------------------------------
+    function showInputWheels (){
+        wheelsInput.classList.remove("invisible");
+        wheelsInput.classList.add("visible");
+        
+    };
 
-    wheelsInfo.innerHTML = "WHEELS: " + JSON.stringify(car.wheels);
+    function checkWheels(){
+        car.addWheel(new Wheel(diameterWheel1, brandWheel1.value));  
+        wheelsOutput.innerHTML = "WHEELS: " + JSON.stringify(car.wheels);
+        
 
-   
+    };
+// END FUNCTIONS ---------------------------------------------------------------------------------
+    
+
+
+
 }
-
